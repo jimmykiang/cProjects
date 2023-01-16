57641e4d00b55298fe9e45e414231b0983773eb3
jimmykiang <castlevanity@gmail.com>
2023-01-16 21:31:22 +0100

    250 function

--- 250/main.c
+++ 250/main.c
@@ -3,6 +3,45 @@
 
 #define SIZE 10
 
+void findRepeatedValues(int *repeatFlag, int *repeatedValuesIndex, int *totalUniqueValues, int (*repeatedValues)[], int (*values)[])
+{
+    for (int i = 0; i < SIZE; i++)
+    {
+        for (int j = 0; j < SIZE; j++)
+        {
+            if (i == j)
+            {
+
+                continue;
+            }
+            // printf("i: %d, j: %d, Value: %d \n", i, j, values[i]);
+            if ((*values)[i] == (*values)[j])
+            {
+
+                *repeatFlag = 1;
+                (*repeatedValues)[*repeatedValuesIndex] = (*values)[i];
+                // printf("Repeated values og: %d \n", (*values)[i]);
+                // *repeatedValuesIndex++ will introduce a bug in the program. *repeatedValuesIndex++ != *repeatedValuesIndex +=1
+                *repeatedValuesIndex += 1;
+                // printf("Single Repeated values index: %d \n", *repeatedValuesIndex);
+                break;
+            }
+        }
+        if (*repeatFlag == 0)
+        {
+            *totalUniqueValues += 1;
+            printf("Unique value %d \n", (*values)[i]);
+        }
+        *repeatFlag = 0;
+    }
+
+    // printf("Repeated values index: %d \n", *repeatedValuesIndex);
+    for (int i = 0; i < SIZE; i++)
+    {
+        printf("Repeated values:: %d \n", (*repeatedValues)[i]);
+    }
+}
+
 int main()
 {
     int values[SIZE] = {5, 7, 3, 4, 5, 6, 8, 9, 10, 3};
@@ -22,32 +61,8 @@ int main()
     int updatedValues[SIZE] = {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1};
     int updatedValuesIndex = 0;
 
-    for (int i = 0; i < SIZE; i++)
-    {
-        for (int j = 0; j < SIZE; j++)
-        {
-            if (i == j)
-            {
+    findRepeatedValues(&repeatFlag, &repeatedValuesIndex, &totalUniqueValues, &repeatedValues, &values);
 
-                continue;
-            }
-            // printf("i: %d, j: %d, Value: %d \n", i, j, values[i]);
-            if (values[i] == values[j])
-            {
-
-                repeatFlag = 1;
-                repeatedValues[repeatedValuesIndex] = values[i];
-                repeatedValuesIndex++;
-                break;
-            }
-        }
-        if (repeatFlag == 0)
-        {
-            totalUniqueValues += 1;
-            printf("Unique value %d \n", values[i]);
-        }
-        repeatFlag = 0;
-    }
     printf("Total unique count: %d \n", totalUniqueValues);
 
     for (int i = 0; i < SIZE; i++)
